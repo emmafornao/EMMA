ac6628af9d07350ece3007d02a7d0eca8f3b2a95
emmafornao <230991366+emmafornao@users.noreply.github.com>
2026-06-30 14:33:31 +0200

    ruff format api handler

--- curseforgeapihandler.py
+++ curseforgeapihandler.py
@@ -3,24 +3,21 @@ import wget
 import pylnk3
 import sys
 
-class API_Handler():
+
+class API_Handler:
     def __init__(self):
 
         api_key_link = pylnk3.parse("data/API_KEY.txt.lnk")
-        with open(api_key_link.path, 'r') as file:
+        with open(api_key_link.path, "r") as file:
             self.api_key = file.read()
 
-        self.headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'x-api-key': self.api_key
-        }
+        self.headers = {"Content-Type": "application/json", "Accept": "application/json", "x-api-key": self.api_key}
 
-        self.url = 'https://api.curseforge.com/v1/'
+        self.url = "https://api.curseforge.com/v1/"
         # self.url = 'https://www.curseforge.com/api/v1/' # used for downloads in browser
 
     def get_mainFileId(self, mod_id):
-        response = requests.get(self.url + 'mods/' + str(mod_id), headers = self.headers)
+        response = requests.get(self.url + "mods/" + str(mod_id), headers=self.headers)
         mod_data = response.json()
         # return mod_data.get("data", []).get("mainFileId", "")
         # https://www.curseforge.com/api/v1/mods/931874/files/6853336/download
@@ -47,7 +44,7 @@ class API_Handler():
 
     def download_mod(self, mod_id, mainfile_id):
         # url = self.url + 'mods/' + str(mod_id) + '/files/' + str(mainfile_id) + '/download'
-        url = 'https://www.curseforge.com/api/v1/mods/' + str(mod_id) + '/files/' + str(mainfile_id) + '/download'
+        url = "https://www.curseforge.com/api/v1/mods/" + str(mod_id) + "/files/" + str(mainfile_id) + "/download"
         """ # response = requests.get(url, headers = self.headers)
         response = requests.get(url)
 
@@ -62,13 +59,13 @@ class API_Handler():
         try:
             print(f"Trying to download mod {mod_id} with main file {mainfile_id}")
             print(f"URL: {url}")
-            filename = wget.download(url, out=f'downloads\\{mod_id}.zip')#, bar=self.progress_bar)
+            filename = wget.download(url, out=f"downloads\\{mod_id}.zip")  # , bar=self.progress_bar)
             print(f"\nDownload complete: {filename}")
         except Exception as e:
             print(f"\nError downloading mod: {e}")
 
     def download_mod_entry(self, mod_id):
-        response = requests.get(self.url + 'mods/' + str(mod_id), headers = self.headers)
+        response = requests.get(self.url + "mods/" + str(mod_id), headers=self.headers)
         if response.status_code == 200:
             mod_cf_data = response.json()
             # mod_library_entry = {}
@@ -76,11 +73,9 @@ class API_Handler():
             return mod_cf_data
         print("Failed to download mod info to update library.json entry.")
 
-
-        
     def mod_search(self, params):
-        r = requests.get(self.url + 'mods/search', params, headers = self.headers)
-        
+        r = requests.get(self.url + "mods/search", params, headers=self.headers)
+
         """ params={
         'gameId': '83374'
         } """
